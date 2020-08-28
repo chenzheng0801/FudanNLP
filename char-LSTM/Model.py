01576d75387f26afa0e3c444d8215e81f75e6107
chenzheng0801 <chenzheng_dut@163.com>
2020-08-29 01:47:10 +0800

    put the standard char-lstm implenmentation into the project

--- char-LSTM/Model.py
+++ char-LSTM/Model.py
@@ -6,17 +6,19 @@ import os
 
 class CharLSTM:
     def __init__(self, batch_size, num_layers, hidden_size, v_size, learning_rate, grad_clip, n_steps):
+        self.batch_size, self.n_steps = batch_size, n_steps
+
         #输入层
         with tf.name_scope("inputs"):
             self.inputs, self.targets, self.keep_prob = self.build_model_input(n_steps, batch_size)
             embed_vec = tf.Variable(tf.random_normal([v_size, hidden_size],
                                                      mean=0, stddev=0.3), name="embed_vec")
-            embedding = tf.nn.embedding_lookup(embed_vec, self.inputs, name="embedding")
+            self.embedding = tf.nn.embedding_lookup(embed_vec, self.inputs, name="embedding")
 
         with tf.name_scope("lstm_layer"):
             cell, self.initial_state = self.build_lstm_layer(batch_size, num_layers,
                                                              hidden_size, self.keep_prob)
-            outputs, state = tf.nn.dynamic_rnn(cell, embedding, initial_state=self.initial_state)
+            outputs, state = tf.nn.dynamic_rnn(cell, self.embedding, initial_state=self.initial_state)
             self.final_state = state
 
         with tf.name_scope("output_layer"):
@@ -26,9 +28,15 @@ class CharLSTM:
         self.train_op = self.build_optimizer(learning_rate, grad_clip)
 
     def build_lstm_layer(self, batch_size, num_layers, hidden_size, keep_prob):
-        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
-        drop_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=keep_prob)
-        cell = tf.nn.rnn_cell.MultiRNNCell([drop_cell for _ in range(num_layers)])
+
+        def get_a_cell(hidden_size, keep_prob):
+            lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_size)
+            drop_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=keep_prob)
+            return drop_cell
+
+        cell = tf.nn.rnn_cell.MultiRNNCell(
+            [get_a_cell(hidden_size, keep_prob) for _ in range(num_layers)]
+        )
         initial_state = cell.zero_state(batch_size, dtype=tf.float32)
         return cell, initial_state
 
@@ -61,12 +69,12 @@ class CharLSTM:
     def build_optimizer(self, learning_rate, grad_clip):
         optimizer = tf.train.AdamOptimizer(learning_rate)
         tvars = tf.trainable_variables()
-        grads = tf.gradients(self.loss, tvars)
-        grads, _ = tf.clip_by_global_norm(grads, grad_clip)
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), grad_clip)
         train_op = optimizer.apply_gradients(zip(grads, tvars))
         return train_op
 
-    def train(self, batch_generator, save_every_n, log_every_n, train_keep_prob, epoches, save_path=None):
+    def train(self, batch_generator, save_every_n, log_every_n,
+              train_keep_prob, epoches, save_path=None):
         step = 0
         saver = tf.train.Saver()
         with tf.Session() as sess:
@@ -74,26 +82,26 @@ class CharLSTM:
             new_state = sess.run(self.initial_state)
             start_time = time.time()
             loss = 0
-            for epoch in range(1, epoches + 1):
-                for batch_x, batch_y in batch_generator:
-                    step += 1
-                    feed = {self.inputs: batch_x,
-                            self.targets: batch_y,
-                            self.keep_prob: train_keep_prob,
-                            self.initial_state: new_state}
-                    batch_loss, new_state, _ = sess.run([self.loss,
-                                                         self.final_state,
-                                                         self.train_op],
-                                                        feed_dict=feed)
-                    loss += batch_loss
-                    if step % log_every_n == 0:
-                        end_time = time.time()
-                        print('step:{}'.format(step),
-                              'epoc: {}/{}'.format(epoch, epoches),
-                              '{} sec/batch'.format((end_time - start_time)/log_every_n),
-                              'loss: {}'.format(loss/log_every_n))
-                        loss = 0
-                        start_time = time.time()
+            for batch_x, batch_y in batch_generator:
+                step += 1
+                feed = {self.inputs: batch_x,
+                        self.targets: batch_y,
+                        self.keep_prob: train_keep_prob,
+                        self.initial_state: new_state}
+                embedding = sess.run([self.embedding], feed_dict=feed)
+                batch_loss, new_state, _ = sess.run([self.loss,
+                                                     self.final_state,
+                                                     self.train_op],
+                                                    feed_dict=feed)
+                loss += batch_loss
+                if step % log_every_n == 0:
+                    end_time = time.time()
+                    print('step:{}'.format(step),
+                          # 'epoc: {}/{}'.format(epoch, epoches),
+                          '{:.4f} sec/batch'.format((end_time - start_time)/log_every_n),
+                          'loss: {:.4f}'.format(loss/log_every_n))
+                    loss = 0
+                    start_time = time.time()
 
-                    if step % save_every_n == 0:
-                        saver.save(sess, os.path.join(save_path, 'model'), global_step=step)
+                if step % save_every_n == 0:
+                    saver.save(sess, os.path.join(save_path, 'model'), global_step=step)
--- char-LSTM/task.py
+++ char-LSTM/task.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os
 import codecs
 
+from data_utils import TextConverter, batch_generator
 from Model import CharLSTM
 from DataProcessing import encode_seq
 from DataProcessing import get_batches
@@ -16,7 +17,7 @@ tf.flags.DEFINE_integer('hidden_size', 128, 'size of hidden state of lstm')
 tf.flags.DEFINE_integer('num_layers', 2, 'number of lstm layers')
 tf.flags.DEFINE_boolean('use_embedding', False, 'whether to use embedding')
 tf.flags.DEFINE_integer('embedding_size', 128, 'size of embedding')
-tf.flags.DEFINE_float('learning_rate', 0.01, 'learning_rate')
+tf.flags.DEFINE_float('learning_rate', 0.001, 'learning_rate')
 tf.flags.DEFINE_float('train_keep_prob', 0.5, 'dropout rate during training')
 tf.flags.DEFINE_string('input_file', 'anna.txt', 'utf8 encoded text file')
 tf.flags.DEFINE_integer('max_steps', 100000, 'max steps to train')
@@ -26,27 +27,33 @@ tf.flags.DEFINE_integer('max_vocab', 3500, 'max char number')
 tf.flags.DEFINE_integer('batch_size', 100, 'batch size of the data')
 tf.flags.DEFINE_float('grad_clip', 5, 'gradient clip threshold')
 
+
 def main(_):
 
     model_path = os.path.join('model', FLAGS.name)
     if os.path.exists(model_path) is False:
         os.makedirs(model_path)
 
-    vocab_to_int, int_to_vocab, encode_text\
-        = encode_seq(FLAGS.input_file)
+    # vocab_to_int, int_to_vocab, encode_text\
+    #     = encode_seq(FLAGS.input_file)
+
+    with codecs.open(FLAGS.input_file, encoding='utf-8') as f:
+        text = f.read()
+    converter = TextConverter(text, FLAGS.max_vocab)
+    arr = converter.text_to_arr(text)
+    g = batch_generator(arr, FLAGS.batch_size, FLAGS.n_steps)
 
     model = CharLSTM(batch_size=FLAGS.batch_size,
                      hidden_size=FLAGS.hidden_size,
                      num_layers=FLAGS.num_layers,
-                     v_size=len(vocab_to_int),
+                     v_size=converter.vocab_size,
                      learning_rate=FLAGS.learning_rate,
                      grad_clip=FLAGS.grad_clip,
                      n_steps=FLAGS.n_steps)
-
-    batches = get_batches(encode_text, FLAGS.batch_size, FLAGS.n_steps, vocab_to_int["\n"])
-    model.train(batches, FLAGS.save_every_n,
+    model.train(g, FLAGS.save_every_n,
                 FLAGS.log_every_n, FLAGS.train_keep_prob,
                 FLAGS.epoches, save_path=model_path)
+
 
 if __name__ == '__main__':
     tf.app.run()
